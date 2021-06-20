ecf144f8d206e4044851d088926e5d3fff2be402
SHREYA V <82759047+ShreyaV-E0320017@users.noreply.github.com>
2021-06-20 13:05:56 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$conn = mysqli_connect('localhost','id17075388_id3845sparks_bank','$&4WgVCb}-c|^jy6','id17075388_sparks_bank');
+
+if(!$conn){
+die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+}
+
+?>
---
+++ navbar.php
@@ -0,0 +1,23 @@
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+<a class="navbar-brand" href="index.php">THE SPARKS FOUNDATION COOPERATIVE</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+<ul class="navbar-nav ml-auto">
+<li class="nav-item">
+<a class="nav-link" href="index.php" style="color: #472864;" >HOME</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="viewallcustomers.php" style="color: #472864;">USERS</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="transfermoney.php" style="color: #472864;">TRANSFER</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="transactionhistory.php" style="color: #472864;">HISTORY</a>
+</li>
+</div>
+</nav>
